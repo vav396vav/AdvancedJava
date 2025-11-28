9b37b703ab048275647857618a61a4268e24ddb2
Александр веденёв <vav396vav@gmail.com>
2025-11-28 17:15:20 +0300

    удалил конец

--- git_version/src/Main.java
+++ git_version/src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!"); // проба
-
+        System.out.println("Hello World!");
     }
 }
