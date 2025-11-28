80134a9b8ee8f17335cf2c8495cd5e610f3c95e7
Александр веденёв <vav396vav@gmail.com>
2025-11-28 14:54:07 +0300

    изменение на пк

--- git_version/src/Main.java
+++ git_version/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        // роба
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        System.out.println("Hello World!"); // проба
     }
 }
