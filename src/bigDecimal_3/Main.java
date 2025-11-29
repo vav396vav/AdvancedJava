af58408cf9a7dd2e2ac243b562e79a548f162fc6
Александр веденёв <vav396vav@gmail.com>
2025-11-29 06:27:59 +0300

    первый ком

---
+++ src/bigDecimal_3/Main.java
@@ -0,0 +1,22 @@
+package bigDecimal_3;
+
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args) {
+        double a = 0.1;
+        double b = 0.2;
+        double c = 0.3;
+        if (a + b == c) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+        BigDecimal a1 = new BigDecimal("0.2");
+        BigDecimal b1 = new BigDecimal(0.2);
+
+        System.out.println(a1.equals(b1));
+        System.out.println(a1 == b1);
+    }
+}
